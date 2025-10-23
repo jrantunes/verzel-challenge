51097c4f450edf6bc56927afd5961b2449873bae
Jose <joseantunes2001@gmail.com>
2025-10-23 17:37:19 -0300

    chore: Initial setup

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import cors from "cors"
+import express from "express"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (_, res) => {
+  res.send("API Rodando")
+})
+
+export default app
