7eb6bf538f070d8368c9868891b47a1b149e6053
Jose <joseantunes2001@gmail.com>
2025-11-06 22:49:27 -0300

    component(frontend): hide genre selection on search

--- frontend/src/features/movies/pages/discover/index.tsx
+++ frontend/src/features/movies/pages/discover/index.tsx
@@ -1,11 +1,15 @@
 import { SearchFilters } from "@/components"
 import styles from "./styles.module.scss"
 import { DiscoverList } from "../components/discover-list"
+import { useRecoilValue } from "recoil"
+import { moviesFilter } from "@/state/atom"
 
 export const DiscoverPage = () => {
+  const filter = useRecoilValue(moviesFilter)
+
   return (
     <div className={styles.wrapper}>
-      <SearchFilters />
+      {!filter.search && <SearchFilters />}
       <DiscoverList />
     </div>
   )
